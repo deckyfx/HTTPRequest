031893563a253e0039325fc487b0922f490ce34c
Decky Fiyemonda <yggdra@ymail.com>
2017-10-09 15:08:38 +0700

    update helper

--- app/src/main/java/com/github/deckyfx/httprequest/KeyValuePair.java
+++ app/src/main/java/com/github/deckyfx/httprequest/KeyValuePair.java
@@ -8,11 +8,26 @@ public class KeyValuePair {
     private String key;
     private String value;
 
+    public KeyValuePair(String key, String value){
+        this.key = key;
+        this.value = value;
+    }
+
     public String getKey() {
         return key;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public KeyValuePair key(String key) {
+        this.key = key;
+        return this;
+    }
+
+    public KeyValuePair value(String value) {
+        this.value = value;
+        return this;
     }
 }
