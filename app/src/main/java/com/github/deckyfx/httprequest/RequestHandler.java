ce048463109e02fa925c1e6fb67a7566231f1d1f
Decky Fiyemonda <yggdra@ymail.com>
2017-10-09 14:19:23 +0700

    change interface param name

--- app/src/main/java/com/github/deckyfx/httprequest/RequestHandler.java
+++ app/src/main/java/com/github/deckyfx/httprequest/RequestHandler.java
@@ -6,10 +6,10 @@ import com.github.deckyfx.okhttp3.Response;
  * Created by decky on 2/2/17.
  */
 public interface RequestHandler {
-    void onHTTPRequestStart(Request callback);
-    void onHTTPRequestFinish(Request callback);
-    void onHTTPRequestSuccess(Request callback, Response response, String responseBody);
-    void onHTTPRequestFailure(Request callback, Throwable error);
-    void onHTTPRequestRescue(Request callback, String recoveredResponse);
-    void onHTTPRequestNetworkError(Request callback);
+    void onHTTPRequestStart(Request request);
+    void onHTTPRequestFinish(Request request);
+    void onHTTPRequestSuccess(Request request, Response response, String responseBody);
+    void onHTTPRequestFailure(Request request, Throwable error);
+    void onHTTPRequestRescue(Request request, String recoveredResponse);
+    void onHTTPRequestNetworkError(Request request);
 }
